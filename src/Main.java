452548d2868a0f7f13a9c563f8a886d7d50436c2
Sweta113Sharma <Sweta.dollysharma@gmail.com>
2026-07-31 00:07:21 +0530

    printing statements

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[]args){
+        //this is my first java programme
+        /*multi line comment
+
+         */
+System.out.println("hi im new here");
+System.out.println("nice to meet you");
+        System.out.println("give me a bag");
+    }
+}
