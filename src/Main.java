fd1f285c3d8dadf66cb9528e4462d610855a6843
Sweta113Sharma <Sweta.dollysharma@gmail.com>
2026-07-31 00:07:21 +0530

    printing statements

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[]args){
+        //this is my first java programme
+        /*multi line comment
+System.out.println("hi im new here");
+System.out.println("nice to meet you");
+        System.out.println("give me a bag");
+        ------1/71------*/
+        int age=20;
+        int year=2025;
+        int quantity=3;
+        System.out.println("the year is " + year);
+        double price=12.5;
+        System.out.println("the weight of the gold is "+price);
+        System.out.println("i have "+quantity+" chains ");
+        System.out.println("i have "+price+" grams of gold");
+
+    }
+}
